095be716162b9cc6eaed49bfa5f7d6f4bfcd7936
lidan233 <2016735055@qq.com>
2021-04-27 22:54:10 +0800

    add cmake

---
+++ geometry/Particle.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 李源 on 2021-04-23.
+//
+
+#include "Particle.h"
---
+++ geometry/Particle.h
@@ -0,0 +1,14 @@
+//
+// Created by 李源 on 2021-04-23.
+//
+
+#ifndef CG_PARTICLE_H
+#define CG_PARTICLE_H
+
+
+class Particle {
+
+};
+
+
+#endif //CG_PARTICLE_H
